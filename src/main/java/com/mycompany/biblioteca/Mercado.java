1c477892cd9921ac321863dc7b0e175c41373cca
barah <barah@LAPTOP-T0P5O1B1>
2024-08-08 15:58:52 -0500

    Se añadio la ventana animales

---
+++ src/main/java/com/mycompany/biblioteca/Mercado.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.biblioteca;
+
+/**
+ *
+ * @author barah
+ */
+public class Mercado {  
+    public static void main(String[] args) {
+     ConexionFirebase.conectarFirabase();
+    }
+}
